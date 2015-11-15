c98021cfd2134e588e509d640d7247ebaf95fd58
benapp <beneapp@gmail.com>
2015-11-15 18:21:39 +0900

    _2015년 11월 15일 18시 16분_2015.11.15

--- test_transitiondemo/src/main/java/com/example/administrator/test_transitiondemo/TransitionDemoActivity.java
+++ test_transitiondemo/src/main/java/com/example/administrator/test_transitiondemo/TransitionDemoActivity.java
@@ -4,14 +4,49 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 public class TransitionDemoActivity extends AppCompatActivity {
+
+    ViewGroup myLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transition_demo);
+
+        // TODO: 2015-11-15  butterknife 를 사용하자
+        myLayout = (ViewGroup) findViewById(R.id.myLayout);
+        // OnTouchListener 에서 맨 앞에 있는 O가 대문자였다. !!
+        myLayout.setOnTouchListener(new RelativeLayout.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent e) {
+                handleTouch();
+                return true;
+            }
+        });
+
     }
+
+    private void handleTouch() {
+        View view = findViewById(R.id.myButton1);
+
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
+        params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+        view.setLayoutParams(params);
+
+        ViewGroup.LayoutParams lparams = view.getLayoutParams();
+
+        lparams.width = 500;
+        lparams.height = 350;
+        view.setLayoutParams(lparams);
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
